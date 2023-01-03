ab24f847643ffe838823c4ad2ac94414411fb12e
Александр Федорович <alex2717@yandex.kz>
2023-01-03 17:29:47 +0600

    регистрация

--- handlers/user/handlers.py
+++ handlers/user/handlers.py
@@ -2,27 +2,25 @@ import sqlite3
 
 from aiogram import types
 
-from config.bot_config import dp, bot
-from handlers.user.new_member import add_new_user_to_db
+from data.config import dp, bot
 
 
 def check_user_in_base(message):
     """Проверяем пользователя на наличие в БД."""
-    conn = sqlite3.connect('random_coffee.db')
+    conn = sqlite3.connect('data/coffee_database.db')
     cur = conn.cursor()
     info = cur.execute(
         """SELECT * FROM users WHERE tg_id=?""", (message.from_user.id,)
     )
-    conn.commit()
     if info.fetchone() is None:
         # Делаем когда нету человека в бд
         return False
     return True
 
-def change_take_part_status_to_yes(message: types.Message):
-    conn = sqlite3.connect('random_coffee.db')
+def change_status_to_yes(message: types.Message):
+    conn = sqlite3.connect('data/coffee_database.db')
     cur = conn.cursor()
-    cur.execute("""update users SET take_part = ? where tg_id = ?""", (
+    cur.execute("""update user_status SET status = ? where tg_id = ?""", (
         1, message.from_user.id
     ))
     conn.commit()
@@ -36,43 +34,6 @@ def change_take_part_status_to_no(message: types.Message):
     ))
     conn.commit()
 
-
-@dp.callback_query_handler(text="take_part")
-async def take_part(message: types.Message):
-    if not check_user_in_base(message):
-        await bot.send_message(message.from_user.id,
-            'Мы не нашли Вас в нашей базе. Пожалуйста ответьте на несколько вопросов и мы добавим Вас в наш список.')
-        await add_new_user_to_db(message)
-    else:
-        change_take_part_status_to_yes(message)
-        await bot.send_message(message.from_user.id, 'Ваша заявка принята. Ожидайте распределения')
-
-@dp.callback_query_handler(text="do_not_take_part")
-async def do_not_take_part(message: types.Message):
-    if not check_user_in_base(message):
-        await message.answer(
-            'Мы не нашли Вас в нашей базе. Пожалуйста ответьте на несколько вопросов и мы добавим Вас в наш список.')
-    change_take_part_status_to_no(message)
-    await bot.send_message(message.from_user.id, 'Вы не будете участвовать в распределении на следующей неделе.')
-
-async def question_about_take_part(tg_id):
-    await bot.send_message(
-        tg_id,
-        text=f'Желаешь принять участие на следующей неделе?',
-        reply_markup=keyboard_choose_take_part_or_not
-    )
-
-keyboard_choose_take_part_or_not = types.InlineKeyboardMarkup()
-
-take_part = types.InlineKeyboardButton(
-    'Участвую',
-    callback_data='take_part'
-)
-do_not_take_part = types.InlineKeyboardButton(
-    'Не участвую',
-    callback_data='do_not_take_part'
-)
-keyboard_choose_take_part_or_not.row(take_part, do_not_take_part)
 
 
 def get_data_from_db_for_mailing_list():
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sqlite3
 from data.config import dp, bot
 from aiogram import executor, types
-from .controllerBD import DatabaseManager
+from controllerBD import DatabaseManager
+from keyboards.user import *
+from states import UserData
+from handlers.user import *
 
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def process_start_command(message: types.Message):
+async def process_start_command(message: types.Message, state: FSMContext):
     """Функция первого обращения к боту."""
     name = message.from_user.full_name
     await bot.send_message(
@@ -21,27 +24,14 @@ async def check_and_add_registration_button(message: types.Message):
         await bot.send_message(
             message.from_user.id,
             text="Нажмите кнопку регистрации для старта.",
-            reply_markup=registration_keyboard
+            reply_markup=start_registr_markup()
         )
-    pass
-
-
-def check_user_in_base(message):
-    """Проверяем пользователя на наличие в БД"""
-    return False
-
-
-registration_keyboard = types.InlineKeyboardMarkup()
-
-registration = types.InlineKeyboardButton(
-    'Регистрация',
-    callback_data='registration'
-)
-registration_keyboard.row(registration)
+        await UserData.start.set()
+    pass # дописать если пользователь в базе при необходимости
 
 
 if __name__ == '__main__':
-    path = '/data/coffee_database.db'
+    path = 'data/coffee_database.db'
     db_controller = DatabaseManager(path)
     db_controller.create_tables()
     executor.start_polling(dp, skip_updates=True)
