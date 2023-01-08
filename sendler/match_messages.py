b15408add926a05c41d8c92ddcde45816fbefcb5
Artem Menshikov <a.menshikov1989@gmail.com>
2023-01-08 23:07:38 +0300

    add logger sendler

--- sendler/match_messages.py
+++ sendler/match_messages.py
@@ -1,9 +1,11 @@
+import logging
 from importlib.machinery import SourceFileLoader
 
 from aiogram import Bot
 from loader import db_controller
 
-clas = SourceFileLoader("module.name", "./controllerBD/manage_db.py").load_module()
+clas = SourceFileLoader("module.name",
+                        "./controllerBD/manage_db.py").load_module()
 clas_1 = SourceFileLoader("module.name", "./data/__init__.py").load_module()
 
 ADMIN_TG_ID = clas_1.ADMIN_TG_ID
@@ -13,48 +15,75 @@ async def send_match_messages(match_info: dict, bot: Bot):
     """Рассылка сообщений после распределения пар на неделю."""
     for match in match_info.items():
         users_info = pare_users_query(match)
-        if len(users_info) == 2:
+        if not users_info:
+            logging.error(f'Не удалось получить информацию '
+                          f'из БД для пары {match}')
+            continue
+        elif len(users_info) == 2:
             first_user = users_info[0]
             first_user_id = first_user[1]
             second_user = users_info[1]
             second_user_id = second_user[1]
-            first_message = make_message(first_user)
-            second_message = make_message(second_user)
+            try:
+                first_message = make_message(first_user)
+            except Exception as error:
+                logging.error(f'Не удалось сформировать сообщение о '
+                              f'пользователе {first_user}. Ошибка {error}')
+            try:
+                second_message = make_message(second_user)
+            except Exception as error:
+                logging.error(f'Не удалось сформировать сообщение о '
+                              f'пользователе {second_user}. Ошибка {error}')
             try:
                 await bot.send_message(second_user_id, first_message,
                                        parse_mode="HTML")
-            except Exception:
-                pass  # TODO сюда добавить логгер
+                logging.info(f'Сообщение для пользователя {second_user_id} '
+                             f'отправлено')
+            except Exception as error:
+                logging.error(f'Сообщение для пользователя {second_user_id} '
+                              f'не отправлено. Ошибка {error}')
             try:
                 await bot.send_message(first_user_id, second_message,
                                        parse_mode="HTML")
-            except Exception:
-                pass  # TODO сюда добавить логгер
+                logging.info(f'Сообщение для пользователя {first_user_id} '
+                             f'отправлено')
+            except Exception as error:
+                logging.error(f'Сообщение для пользователя {first_user_id} '
+                              f'не отправлено. Ошибка {error}')
         else:
             fail_user = users_info[0]
             fail_user_id = fail_user[1]
-            fail_user_name = fail_user[2]
-            message = (f'Без пары на этой неделе: <a href="tg://user?id'
-                       f'={fail_user_id}">{fail_user_name}</a>')
+            message = make_message(fail_user, fail=True)
             try:
                 await bot.send_message(ADMIN_TG_ID,
                                        message, parse_mode="HTML")
-            except Exception:
-                pass  # TODO сюда добавить логгер
+                logging.info(f'Сообщение админу об отсутствии пары '
+                             f'для пользователя {fail_user_id} '
+                             f'отправлено')
+            except Exception as error:
+                logging.error(f'Сообщение админу об отсутствии пары '
+                              f'для пользователя {fail_user_id} '
+                              f'не отправлено. Ошибка {error}')
 
 
-def make_message(user_info: tuple):
+def make_message(user_info: tuple, fail: bool = False):
     """Формирует строку для отправки."""
-    user_id = user_info[1]
-    user_name = user_info[2]
-    user_birthday = user_info[3]
-    user_about = user_info[4]
-    user_gender = user_info[5]
-    message = (f'На этой неделе Ваша пара для кофе: '
-               f'<a href="tg://user?id={user_id}">{user_name}</a> \n'
-               f'Дата рождения: {user_birthday}\n'
-               f'Информация: {user_about}\n'
-               f'Пол: {user_gender}\n')
+    if not fail:
+        user_id = user_info[1]
+        user_name = user_info[2]
+        user_birthday = user_info[3]
+        user_about = user_info[4]
+        user_gender = user_info[5]
+        message = (f'На этой неделе Ваша пара для кофе: '
+                   f'<a href="tg://user?id={user_id}">{user_name}</a>\n'
+                   f'Дата рождения: {user_birthday}\n'
+                   f'Информация: {user_about}\n'
+                   f'Пол: {user_gender}\n')
+    else:
+        fail_user_id = user_info[1]
+        fail_user_name = user_info[2]
+        message = (f'Без пары на этой неделе: <a href="tg://user?id'
+                   f'={fail_user_id}">{fail_user_name}</a>')
     return message
 
 
@@ -72,6 +101,6 @@ def pare_users_query(pare: tuple):
     try:
         result = db_controller.select_query(query, pare).fetchall()
     except Exception:
-        result = None  # TODO сюда добавить логгер
+        result = None
     finally:
         return result
