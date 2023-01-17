caef40f3dd8dd5a36665c248b92b0571c318d874
a-menshikov <a.menshikov1989@gmail.com>
2023-01-17 23:41:15 +0300

    update logger

--- loader.py
+++ loader.py
@@ -2,19 +2,29 @@ import logging
 
 from data import config
 from controllerBD import DatabaseManager
+from logging.handlers import RotatingFileHandler
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-logging.basicConfig(filename="main.log",
-                    filemode='a',
-                    format=('%(asctime)s,%(msecs)d %(name)s '
-                            '%(levelname)s %(message)s'),
-                    datefmt='%H:%M:%S',
-                    level=logging.INFO)
-
 logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+handler = RotatingFileHandler('my_logger.log', maxBytes=50000000,
+                              backupCount=5)
+
+logger.addHandler(handler)
+
+formatter = logging.Formatter(
+    fmt=('%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s'),
+    datefmt='%H:%M:%S'
+)
+
+handler.setFormatter(formatter)
+
 bot = Bot(token=str(config.TOKEN))
 dp = Dispatcher(bot, storage=MemoryStorage())
+
 path = 'data/coffee_database.db'
 db_controller = DatabaseManager(path, logger)
 db_controller.create_tables()
