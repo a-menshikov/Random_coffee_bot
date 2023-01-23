426deda63f6a9aa562d81691e5057ea37f709b4c
a-menshikov <a.menshikov1989@gmail.com>
2023-01-23 12:54:24 +0300

    добавлены новые переменные - тг_id дефолтного юзера для встреч и его username

--- data/config.py
+++ data/config.py
@@ -6,3 +6,5 @@ load_dotenv()
 
 TOKEN = os.getenv('TG_TOKEN')
 ADMIN_TG_ID = os.getenv('ADMIN_TG_ID')
+DEFAULT_PARE_iD = os.getenv('DEFAULT_PARE_iD')
+DEFAULT_PARE_USERNAME = os.getenv('DEFAULT_PARE_USERNAME')
