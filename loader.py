2509d70a42611c755d30639becf3b10827d667ac
a-menshikov <a.menshikov1989@gmail.com>
2023-02-12 23:50:41 +0300

    Доработан формат логгера, установлена московская таймзона для логгера

--- loader.py
+++ loader.py
@@ -1,6 +1,8 @@
 import logging
+from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
+import pytz
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
@@ -8,16 +10,23 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from controllerBD import DatabaseManager
 from data import config
 
-logger = logging.getLogger(__name__)
+timezone = pytz.timezone('Etc/GMT-3')
+
+
+def timetz(*args):
+    return datetime.now(timezone).timetuple()
+
+
+logger = logging.getLogger('main_logger')
 
 aiogram_logger = logging.getLogger('aio_logger')
 
 logger.setLevel(logging.INFO)
-aiogram_logger.setLevel(logging.DEBUG)
+aiogram_logger.setLevel(logging.INFO)
 
-main_handler = RotatingFileHandler('my_logger.log', maxBytes=50000000,
+main_handler = RotatingFileHandler('my_logger.log', maxBytes=30000000,
                                    backupCount=5)
-aiogram_handler = RotatingFileHandler('aiogram_logger.log', maxBytes=50000000,
+aiogram_handler = RotatingFileHandler('aiogram_logger.log', maxBytes=30000000,
                                       backupCount=2)
 
 
@@ -25,9 +34,12 @@ logger.addHandler(main_handler)
 aiogram_logger.addHandler(aiogram_handler)
 
 formatter = logging.Formatter(
-    fmt=('%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s'),
-    datefmt='%H:%M:%S'
+    fmt=('%(asctime)s.%(msecs)d %(levelname)s '
+         '%(filename)s %(funcName)s %(message)s'),
+    datefmt='%d-%m-%Y %H:%M:%S',
 )
+
+formatter.converter = timetz
 
 main_handler.setFormatter(formatter)
 aiogram_handler.setFormatter(formatter)
