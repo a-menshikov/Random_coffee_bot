4e81d39bf254959aefc58ddad75f76044b172f5b
Aleksandr140590 <alex2717@yandex.kz>
2023-02-16 22:06:44 +0600

    alchemy

--- base_and_services/models.py
+++ base_and_services/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Column, ForeignKey, Date, Boolean
+from sqlalchemy import Integer, String, Column, ForeignKey, Date, Boolean, Text
 from datetime import date
 
 from base_and_services.db_loader import engine, Base
@@ -7,16 +7,16 @@ from base_and_services.db_loader import engine, Base
 class Gender(Base):
     __tablename__ = 'genders'
     id = Column(Integer, primary_key=True, unique=True)
-    gender_name = Column(String(20), nullable=False, unique=True)
+    gender_name = Column(Text(100), nullable=False, unique=True)
 
 
 class Users(Base):
     __tablename__ = 'user_info'
     id = Column(Integer, primary_key=True, autoincrement=True)
     teleg_id = Column(Integer, nullable=False, unique=True)
-    name = Column(String(100), nullable=False)
-    birthday = Column(Date(), nullable=True)
-    about = Column(String(500), nullable=True)
+    name = Column(Text(100), nullable=False)
+    birthday = Column(Text(), nullable=False)
+    about = Column(Text(500), nullable=False)
     gender = Column(Integer, ForeignKey('genders.id'))
 
 
@@ -24,18 +24,18 @@ class BanList(Base):
     __tablename__ = 'ban_list'
     id = Column(Integer, primary_key=True, autoincrement=True)
     banned_user_id = Column(Integer, ForeignKey('user_info.id'))
-    ban_status = Column(Boolean, nullable=False)
-    date_of_ban = Column(Date, default=date.today())
-    comment_to_ban = Column(String(500), nullable=False)
-    date_of_unban = Column(Date, nullable=True, default=None)
-    comment_to_unban = Column(String(500), nullable=True, default=None)
+    ban_status = Column(Integer, nullable=False, default=1)
+    date_of_ban = Column(Text(), default=str(date.today()))
+    comment_to_ban = Column(Text(500), nullable=False)
+    date_of_unban = Column(Text(), nullable=False, default='null')
+    comment_to_unban = Column(Text(500), nullable=False, default='null')
 
 
 class Holidays(Base):
     __tablename__ = 'holidays_status'
     id = Column(Integer, ForeignKey('user_info.id'), primary_key=True)
-    status = Column(Boolean, nullable=False, default=0)
-    till_date = Column(Date, nullable=True, default=None)
+    status = Column(Integer, nullable=False, default=0)
+    till_date = Column(Text, nullable=False, default='null')
 
 
 class MetInfo(Base):
@@ -43,30 +43,31 @@ class MetInfo(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_user_id = Column(Integer, ForeignKey('user_info.id'))
     second_user_id = Column(Integer, ForeignKey('user_info.id'))
-    date = Column(Date, default=date.today())
+    date = Column(Text(), default=str(date.today()))
 
 
-class MetsReviews(Base):
+class MetsReview(Base):
     __tablename__ = 'mets_reviews'
     id = Column(Integer, primary_key=True, autoincrement=True)
     met_id = Column(Integer, ForeignKey('met_info.id'))
     who_id = Column(Integer, ForeignKey('user_info.id'))
     about_whom_id = Column(Integer, ForeignKey('user_info.id'))
     grade = Column(Integer, nullable=False)
-    comment = Column(String(500), nullable=True)
-    date = Column(Date, default=date.today(), onupdate=date.today())
+    comment = Column(Text(500), nullable=True)
+    date = Column(Text(), default=str(date.today()),
+                  onupdate=str(date.today()))
 
 
 class UserMets(Base):
     __tablename__ = 'user_mets'
     id = Column(Integer, ForeignKey('user_info.id'), primary_key=True)
-    met_info = Column(String(), nullable=False, default='{}')
+    met_info = Column(Text(), nullable=False, default='{}')
 
 
 class UserStatus(Base):
     __tablename__ = 'user_status'
     id = Column(Integer, ForeignKey('user_info.id'), primary_key=True)
-    status = Column(Boolean, nullable=False, default=1)
+    status = Column(Integer, nullable=False, default=1)
 
 
 async def create_tables():
